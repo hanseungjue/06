584f0fa7681a94dbb158dacc92a617c9912399df
hanseungjue <hansj0628@naver.com>
2022-10-11 17:43:21 +0900

    task5

--- main.c
+++ main.c
@@ -2,16 +2,45 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int square( int a )
+int get_integer1()
 {
-   return (a *a);
+	int n;
+	printf("n을 입력하시오:\n");
+	scanf("%d",&n);
+	return n; 
 }
-
-int main()
+int get_integer2()
 {
-    int a = 2;
-    a = square(a);
-    printf("a=%i\n",a);
+	int r;
+	printf("r을 입력하시오:\n");
+	scanf("%d",&r);
+	return r; 
+}
+int factorial(int n)
+{
+	int i;
+	int res=1;
+	for (i=1;i<n+1;i++)
+	    res=res*i;
+	return res;
+}
+int combination()
+{
+	int n;
+	int r;
+	n=get_integer1();
+    r=get_integer2();
+	int a,b,c;
+	a=factorial(n);
+	b=factorial(n-r);
+	c=factorial(r);
+	return(a/(b*c));
+}
+int main(void)
+{
+    int x;
+    x=combination();
+    printf("%d",x);
 	return 0;
 }
 
