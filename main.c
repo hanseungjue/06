f91fd0e5f616d88f8e46170383057f668a363afe
hanseungjue <hansj0628@naver.com>
2022-10-11 16:42:37 +0900

    task2-1

--- main.c
+++ main.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void func2(void) {
+    int x;
+    printf("func2 x is at %p\n", &x);
+}
 void func(void) {
     int x;
 	printf("func x is at %p\n", &x);
+	func2();
 }
 int main(int argc, char *argv[]) {
     int x;
